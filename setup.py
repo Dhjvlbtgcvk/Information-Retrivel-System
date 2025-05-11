7452313b95328411e5b6681e35f8198db561ca6e
shahzaman <gallanizaman@gmail.com>
2025-05-11 11:15:01 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI End to End Project",
+    version = '0.0.0',
+    
+    author = "Zaman",
+    author_email = "gallanizaman@gmail.com",
+    find_packages = find_packages(),
+    install_requires = []
+)
+
